77ba4bb52fc842d2b1d0c17678c3d299519d242a
xgamereQ <62610958+xgamereQ@users.noreply.github.com>
2022-05-30 21:00:48 +0200

    Delete handlers directory

--- handlers/command.js
+++
@@ -1,9 +0,0 @@
-const { readdirSync } = require("fs");
-module.exports = client => {
-  readdirSync("./commands/").forEach(folder => {
-    readdirSync(`./commands/${folder}/`).forEach(file => {
-	  const dir = `${process.cwd()}/commands/${folder}/${file}`;
-      client.loader.load({file, folder, dir});
-    });
-  });
-};
