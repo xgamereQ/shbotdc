98e1fc72c2853362a4dede647580b3a860d6daf0
xgamereQ <62610958+xgamereQ@users.noreply.github.com>
2022-05-30 21:01:17 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    token: "OTcyOTE4NjE4OTE0NDMwOTc2.GVt6qr.R-WxZ1uxpzVqZ_SP5x_dCM_rkUasb8X-yQOGnU",
-    dev: ["937651916761079810"],
-    settings: {
-        prefix: "$",
-    }
-};
