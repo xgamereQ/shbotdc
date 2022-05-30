f1555b6ff3e8d2a1f16dd676f001bdc6817b3348
xgamereQ <62610958+xgamereQ@users.noreply.github.com>
2022-05-30 20:42:38 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = client => {
     console.log(`Zalogowano jako ${client.user.tag}`);
-    client.user.setActivity('Slowcraft.pl', { type: 'PLAYING'})
+    client.user.setActivity('ShCode.PL', { type: 'PLAYING'})
   };
   
